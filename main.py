99381a66f85197a5680698762f63e2f5bab187a5
huangizi <3478814759@qq.com>
2023-05-17 21:08:16 +0800

    set api-key

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
 # 设置openai的api key
-os.environ['OPENAI_API_KEY'] = "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"
 os.environ["http_proxy"] = "http://127.0.0.1:7890"
 os.environ["https_proxy"] = "http://127.0.0.1:7890"
 
 llm = OpenAI(temperature = 0.9)
 
+# 1.use llms to generate text
 text = "what are 5 vacation destinations for someone who likes to go skiing?"
 print(llm(text))
 
+# 2.promt management
 
 
-## 常见问题
-如果openai访问超时，一定要记得加这两行，具体的端口要在你自己的代理软件里面查看
-os.environ["http_proxy"] = "http://127.0.0.1:7890"
-os.environ["https_proxy"] = "http://127.0.0.1:7890"
+
+
