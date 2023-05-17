0548b4d20f78e8a4717eabc85438c87d6d63ecd3
huangizi <3478814759@qq.com>
2023-05-17 20:59:08 +0800

    first setting

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ llm = OpenAI(temperature = 0.9)
 
 text = "what are 5 vacation destinations for someone who likes to go skiing?"
 print(llm(text))
+
+
+
+## 常见问题
+如果openai访问超时，一定要记得加这两行，具体的端口要在你自己的代理软件里面查看
+os.environ["http_proxy"] = "http://127.0.0.1:7890"
+os.environ["https_proxy"] = "http://127.0.0.1:7890"
